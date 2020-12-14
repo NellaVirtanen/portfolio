8fbe64743f066b71834e109ccdfecef254004c69
NellaVirtanen <nellu.virtanen@gmail.com>
2020-12-14 08:58:33 +0000

    taulukko

--- taulukko.js
+++ taulukko.js
@@ -20,20 +20,20 @@ xhr.send();
 }
 
 getJSON('https://api.apify.com/v2/datasets/BDEAOLx0DzEW91s5L/items?format=json&clean=1', function(err, data){
-let taulukko = `<table width="100%" style="border=1px solid black">`;
+let taulukko = `<table width="60%" style="border:1px solid black">`
 
 const historia = data.map(function(paiva){
 
 if(paiva.confimedCases != undefined){
-console.log(`Confirmed: ${paiva.confirmedCases}`);
+//console.log(`Confirmed: ${paiva.confirmedCases}`);
 taulukko = taulukko + `<tr><td>Vahvistetut</td><td>${paiva.confirmedCases}</td></tr>`;
 }
 else if (paiva.testedCases != undefined){
-console.log(`Tested: ${paiva.testedCases}`);
+//console.log(`Tested: ${paiva.testedCases}`);
 taulukko = taulukko + `<tr><td>Testatut</td><td>${paiva.testedCases}</td></tr>`;
 }
 else if (paiva.infected != undefined){
-console.log(`Infected: ${paiva.infected}`);
+//console.log(`Infected: ${paiva.infected}`);
 taulukko = taulukko + `<tr><td>Tartunnat</td><td>${paiva.infected}</td></tr>`;
 }
 else{
